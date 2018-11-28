63c88255f4786c73de8a82b013a3d0b2d1713a53
Sebastien Bovo <sebastienbovo@outlook.com>
2018-11-28 10:20:06 +0100

    FOV feedback in place

---
+++ FOV-Feedback/Assets/FOV-Feedback/scripts/FOVFeedback.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Interface gives one method to implement. This method modifies the text to display in Unity
+/// Any code can then call this method outside the Unity MonoBehavior object
+/// </summary>
+public interface IUnityFOVFeedback
+{
+    void ModifyOutputText(string newText);
+}
+public class FOVFeedback : MonoBehaviour {
+
+    // Unity 3D Text object that contains 
+    // the displayed TextMesh in the FOV
+    public GameObject OutputText;
+    // TextMesh object provided by the OutputText game object
+    private TextMesh OutputTextMesh;
+    // string to be affected to the TextMesh object
+    private string OutputTextString = string.Empty;
+    // Indicate if we have to Update the text displayed
+    private bool OutputTextChanged = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        OutputTextMesh = OutputText.GetComponent<TextMesh>();
+        OutputTextMesh.text = string.Empty;
+        ModifyOutputText("Hello :-)");
+    }
+
+    /// <summary>
+    /// Modify the text to be displayed in the FOV
+    /// or/and in the debug traces
+    /// + Indicate that we have to update the text to display
+    /// </summary>
+    /// <param name="newText">new string value to display</param>
+    public void ModifyOutputText(string newText)
+    {
+        OutputTextString = newText;
+        OutputTextChanged = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OutputTextChanged)
+        {
+            OutputTextMesh.text = OutputTextString;
+            OutputTextChanged = false;
+        }
+    }
+}
